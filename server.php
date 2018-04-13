2d3332879e4badfa11deaa46a75d76b842f8243f
Fedde Murua <fedde.murua@gmail.com>
2018-04-13 18:47:08 -0300

    comienzo 4vos

--- server.php
+++ server.php
@@ -8,5 +8,7 @@
     echo json_encode(json_decode(file_get_contents("datos/fixture.json"), true));
   } elseif ($fase == "octavos") {
     echo json_encode(json_decode(file_get_contents("datos/8vos.json"), true));
+  } elseif ($fase == "cuartos") {
+    echo json_encode(json_decode(file_get_contents("datos/4vos.json"), true));
   }
 ?>
